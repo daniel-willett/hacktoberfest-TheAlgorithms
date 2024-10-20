fb2335291eba3851f579532d059317849106a559
daniel-willett <willettdr1@gmail.com>
2024-10-20 12:41:58 +0100

    Have added some example cases of valid and invalid IPv6 addresses. These will be used as tests once complete. Have created a general skeleton that should pass standard IPv6 without IPv4 embedding however, has not been tested yet. This commit is just to put down initial thoughts. I'm sure the code isn't that tidy at the moment however, this isn't the finished produce. For now, I just need *something* to be written down which works and then that can be improved on later
    Little more detail on how the code works:
    When a double colon appears for example, "123:23:323::12" then .split(":") should cause one of the blocks to be empty, ["123","23","323","","12"]
    The complexity comes from the fact that when you have the double colon trailing or leading  then an extra empty block is created for example, "x::" or "::x" makes ["x","",""] or ["","","x"] (respectively)
    In valid cases this only happens if the 2 empty blocks are next to each other. So we have a counter for number of empty blocks we've seen, `number_of_empty`, and once we've seen a second empty block then we check if it came straight after the first one.
    
    During commit thought:
    	I've just realised that for example "x1::x2:x3::x4" would return True when should be seen as invalid. Perhaps need another bit of logic to say if `number_of_empty` exceeds 1 then we return False.

--- maths/is_ip_v6_address_valid.py
+++ maths/is_ip_v6_address_valid.py
@@ -1,14 +1,41 @@
 """
 After seeing is_ip_v4_address_valid.py it only seems appropriate that there should exist an equivalent one for IPv6.
-I am going off https://www.ibm.com/docs/en/i/7.4?topic=concepts-ipv6-address-formats and https://www.rfc-editor.org/rfc/rfc4291#section-2.2
+I am going off the following:
+    https://www.ibm.com/docs/en/i/7.4?topic=concepts-ipv6-address-formats
+    https://www.rfc-editor.org/rfc/rfc4291#section-2.2
+    https://www.ibm.com/docs/en/ts4500-tape-library?topic=functionality-ipv4-ipv6-address-formats
 
 Skeleton:
-    Basic addresses are of the form x1:x2:x3:x4:x5:x6:x7:x8 where each xi is a 16 bit number in hexidecimal notation
+    Basic addresses are of the form x1:x2:x3:x4:x5:x6:x7:x8 where each xi segment is a 16 bit number in hexidecimal notation
 
 Rules:
-    Trailing 0's are ok so long as they dont' imply a number larger than 16 bits although if it becomes too hard to code then ideally we don't want trailing 0's
-    Notation can be shortened where consecutive sets of 0's can be replaced with a `::` (replacing an arbitrary number of 0's). A `::` can only appear at most once. This can be used on leading or trailing 0's.
-    An address can have IPv4 embedding via the form x1:x2:x3:x4:x5:x6:d1.d2.d3.d4 where each xi is a 16 bit numebr and each dj is an 8 bit number.
+    Trailing 0's are ok so long as they dont' imply the segment to being a number larger than 16 bits although if it becomes too hard to code then ideally we don't want trailing 0's
+    Notation can be shortened where consecutive segments of 0's can be replaced with a `::` (replacing an arbitrary number of 0's). A `::` can only appear at most once. This can be used on leading or trailing 0's of the address and includes the whole address being 0's.
+    An address can have IPv4 embedding via the form x1:x2:x3:x4:x5:x6:d1.d2.d3.d4 where each xi is a 16 bit numebr and each dj is an 8 bit number. The same normal rules for double colon applies in the first 6 segmetns whilst the  
+
+Valid cases:
+    0:0:0:0:0:0:0:1
+    ::1
+    1::
+    :: (is the same as all zeros)
+    FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF:FFFF
+    98a:329:adae::23:0:0:1
+    1f5e:8fbe:e550:7a07:4679:14fc:1.2.3.4
+    ::1.2.3.4 (the implication is 6 segments of 0's and then the IPv4 embedding)
+    1::1:1.2.3.4
+    132::0.0.0.0
+
+
+Invalid cases:
+    0
+    dasjkhdj
+    0:0:0:0:0:0:0:0:0 (too many segments)
+    0:0:0:0:0:0:0:1.2.3.4 (too many segments)
+    0:0:0:0:0:0:1.2.3.4.5 (too many octets)
+    12:12.12.32.4 (too few segments)
+    10000:0:0:0:0:0:0:0 (too big value)
+    
+
 
 """
 
@@ -21,15 +48,44 @@ def is_ip_v6_address_valid(ip_v6_address: str) -> bool:
         #The smallest address representation is "::" which is 3 blocks and the largest is 8 semicolons which gives 9 blocks)
         return False
 
+    number_of_empty = 0 
+    index = 0
+    for segment in blocks:
+        if (segment == ""):
+            if (number_of_empty == 1) and (blocks[index-1]!=""): #If we've seen a double colon already and the previous block wasn't caused from "::" or "::x" or "x::"
+                return False
+            number_of_empty += 1
+        else:
+            if (is_16_bit_number(segment)==False):
+                return False
+
+        index += 1
+
+        return True
+
+def is_16_bit_number(x: str) -> bool:
+    #Remember, this is in hexedecimal which means base 16 [0-F]
+    try:
+        n = int(x,16) #Convert from base 16 to integer
+        return len(x) <= 4 
+        """
+        Being 16 bit in hex means at most 4 characters. e.g. 01234 implies a number which requires more than 16 bits to represent. Given 2*16 is FFFF which is the largest you can have for 4 characters we don't need to check n < 2**16 as this will always be the case if len(x) <= 4.
+        If we were checking e.g. IPv4 octets we would need to make sure that n < 2**8 as well as e.g. x="256" would certainly give len(x) <= 3 but this isn't a valid octet value because n = int(x,10) = 2**8
+        """
+    except:
+        #It wasn't valid hex
+        return False
+
+
+
+print(is_16_bit_number(""))
+print(is_16_bit_number("AFD2"))
+print(is_16_bit_number("1"))
+print(is_16_bit_number("001"))
+print(is_16_bit_number("00001"))
+print(is_16_bit_number("12345"))
 
 
 
 
-    def is_16_bit_number(x: str) -> bool:
-        #Remember, this is in hexedecimal which means base 16 [0-F]
-        try:
-            n = int(x,16) #Convert from base 16 to integer
-            return n < 2**16 #Being 16 bit means being less than 2^16 (in decimal)
-        except:
-            #It wasn't valid hex
-            return False
+
